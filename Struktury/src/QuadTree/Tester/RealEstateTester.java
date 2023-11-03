ef41bed013a9a745a1630aecf436bcce3a78954a
eva-haluskova <evkahal@gmail.com>
2023-11-03 14:07:15 +0100

    fenerator real estat

---
+++ Struktury/src/QuadTree/Tester/RealEstateTester.java
@@ -0,0 +1,53 @@
+package QuadTree.Tester;
+
+import QuadTree.Coordinates;
+import QuadTree.QuadTree;
+import Data.RealEstate;
+import QuadTree.Data;
+
+import java.util.Random;
+
+public class RealEstateTester extends QuadTreeTester {
+
+    public RealEstateTester(QuadTree parTree) {
+        super(parTree);
+        this.testTree = parTree;
+    }
+
+
+    @Override
+    public void generateInsert(int parNumber, Random random, Coordinates parCoordinatesForSearch) {
+        int widthOfPolygon = 10;
+        dataToFindInArea.clear();
+        dataToFindOfPoint.clear();
+        testData.clear();
+
+        // pay attention, this assume that tree starts with [0,0] point!!
+        double sizeOfXAxes = this.testTree.getRangeOfTree().getUpperX() - this.testTree.getRangeOfTree().getLowerX();
+        double sizeOfYAxes = this.testTree.getRangeOfTree().getUpperY() - this.testTree.getRangeOfTree().getLowerY();
+
+        for (int i = 1; i <= parNumber; i++) {
+            double x1 = random.nextDouble() * (sizeOfXAxes - widthOfPolygon - 1);
+            double x2 = x1 + widthOfPolygon;
+            double y1 = random.nextDouble() * (sizeOfYAxes - widthOfPolygon - 1);
+            double y2 = y1 + widthOfPolygon;
+
+            Coordinates coors = new Coordinates(x1, x2, y1, y2);
+            RealEstate newObject = new RealEstate("RealEstate number3",i);
+            Data<RealEstate> data = new Data(newObject, coors, i);
+
+            this.testTree.insert(data, testTree.getRoot());
+            super.testData.add(data);
+
+            if (super.belongToArea(parCoordinatesForSearch,data.getCoordinates())) {
+                dataToFindInArea.add(data);
+            }
+
+            if (parCoordinatesForSearch.equals(data.getCoordinates())) {
+                dataToFindOfPoint.add(data);
+            }
+        }
+
+        System.out.println("Insert of RealEstates is done!");
+    }
+}
